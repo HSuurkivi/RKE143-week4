93dc0c7981b3f2452a3bfdf45fd1a9f3c4bbb44e
Heleri Suurkivi <heleri.suurkivi@tktk.ee>
2023-10-11 18:30:22 +0300

    week 4 code

---
+++ server.js
@@ -0,0 +1,34 @@
+const kool = {
+    nimi: "Hogwarts",
+    asukoht: {
+      linn: "Hogsmeade",
+      maakond: "Šoti Kõrge-Šotimaa",
+      riik: "Suurbritannia"
+    },
+    tudeng: [
+      {
+        eesnimi: "Harry",
+        perenimi: "Potter",
+        majad: ["Gryffindor", "Kolmas aasta"]
+      },
+      {
+        eesnimi: "Hermione",
+        perenimi: "Granger",
+        majad: ["Gryffindor", "Kolmas aasta"]
+      },
+      {
+        eesnimi: "Ronald",
+        perenimi: "Weasley",
+        majad: ["Gryffindor", "Kolmas aasta"]
+      }
+    ]
+  };
+
+
+const {tudeng: students} = kool;
+const [harry, hermione] = students;
+
+const {eesnimi: harryFirstName} = harry;
+const {eesnimi: hermioneFirstname} = hermione;
+
+console.log(`${harryFirstName} and ${hermioneFirstname} are friends.`);
